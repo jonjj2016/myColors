cffee84b8d17ded6ab3a00fd14b58d677eb39c27
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2020-05-24 13:35:10 -0700

    add copyToClipBoard

--- src/Components/ColorBox/CororBox.jsx
+++ src/Components/ColorBox/CororBox.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { ColorBox } from './Styled';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const CororBox = ({ background, name }) => {
   return (
-    <ColorBox background={background}>
-      <div className='copy-container'>
-        <div className='box-content'>
-          <span>{name} </span>
+    <CopyToClipboard text={background}>
+      <ColorBox background={background}>
+        <div className='copy-container'>
+          <div className='box-content'>
+            <span>{name} </span>
+          </div>
+          <button className='copy-button'>Copy</button>
         </div>
-        <button className='copy-button'>Copy</button>
-      </div>
-      <span className='see-more'>More</span>
-    </ColorBox>
+        <span className='see-more'>More</span>
+      </ColorBox>
+    </CopyToClipboard>
   );
 };
 
